b8f9183ca964ff3717cb22182e5c0fa3d496666d
Flavio Bueno <flavio.bueno86@gmail.com>
2021-11-10 10:01:37 -0300

    Subindo os codigos para o git

---
+++ progressbar.php
@@ -0,0 +1,41 @@
+<?php
+ 
+     
+      //documentação -> http://github.hubspot.com/pace/docs/welcome/
+      //Deverá ser alimentando dentro do  onScriptinit chamando a função "fn_ProgressBar();"
+     //HABILITAR NO PROJETO >> Programação >> Bibliotecas Internas >> habilitar a função
+    
+    //Vários tipos para usar:
+  //pace-theme-barber-shop.css
+    //pace-theme-big-counter.css
+    //pace-theme-bounce.css
+     //pace-theme-center-atom.css
+    //pace-theme-center-circle.css
+    //pace-theme-center-radar.css
+    //pace-theme-center-simple.css
+     //pace-theme-corner-indicator.css
+   //pace-theme-fill-left.css
+    //pace-theme-flash.css
+     //pace-theme-flat-top.css
+   //pace-theme-loading-bar.css
+    //pace-theme-mac-osx.css
+    //pace-theme-minimal.css
+    
+    //public_html/multfoto/loja.v.0.7.0/_lib/libraries/sys/ProgressBar
+    
+    //<script src="../../../devel/conf/sys/libraries/ProgressBar/pace.js"></script>
+     //<link href="../../../devel/conf/sys/libraries/ProgressBar/themes/pace-theme-center-circle.css" rel="stylesheet"/>
+   
+    function fn_ProgressBar()
+   {
+         ?>
+             <head>
+              <script src="../_lib/libraries/sys/ProgressBar/pace.js"></script>
+                 <link href="../_lib/libraries/sys/ProgressBar/themes/pace-theme-center-circle.css" rel="stylesheet"/>
+            </head>
+
+         <?php
+
+   }
+
+ ?>
